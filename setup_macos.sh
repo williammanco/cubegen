d6d1320fc8ffa7d0a083ba7e574040fec862b29f
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-09-05 19:33:59 +0200

    validate tgz

--- setup_macos.sh
+++ setup_macos.sh
@@ -15,7 +15,7 @@ setup() {
     UNAME=$(uname)
 
     if [ "$UNAME" != "Darwin" ] ; then
-        echo "Currently only MacOS is supported by this automatic setup"
+        echo "Currently only MacOS is supported by this automatic setup script"
         exit 1
     fi
     
@@ -33,17 +33,20 @@ setup() {
     # Fetch the latest Filament MacOS release version (Darwin)
     wget $FILAMENT_URL -P bin -O ./bin/filament.tgz
     
-    # Extract the compressed archive
-    # tar -xvzf ./bin/filament.tgz -C bin
+    # Validate the retrieved tgz archive (does not check the integrity of the file)
+    gzip -t ./bin/filament.tgz && echo "Filament was retrieved correctly and the archive is valid" || $(echo "Failed to retrieve Filament correctly and the archive is corrupted"; exit 1)
 
-    # log "Extracing 'cmgen' from latest Filament release"
+    # Extract the compressed archive
+    tar -xvzf ./bin/filament.tgz -C bin
+
+    log "Extracing 'cmgen' from latest Filament release"
 
     # Move the `cmgen` binary into in main `bin` directory
-    # mv ./bin/filament/bin/cmgen bin
+    mv ./bin/filament/bin/cmgen bin
 
     # Clean Filament directory and compressed directory
-    # rm -rf ./bin/filament
-    # rm -rf ./bin/filament.tgz
+    rm -rf ./bin/filament
+    rm -rf ./bin/filament.tgz
 }
 
 # Main script
