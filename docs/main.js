cbc3da025f8bfcdeffbbdedbec206d7b7f8ce8b3
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-09-16 20:37:48 +0200

    added licence info

--- docs/main.js
+++ docs/main.js
@@ -25,10 +25,12 @@ function createWebWorkerInstance(content) {
   return worker;
 }
 
-// Webworker
-const binpackLoader = binpackPath => `
-    const radiancePattern = '#\\?RADIANCE';
-    const commentPattern = '#.*';
+const parseHDR = `
+    // The MIT License (MIT)
+    // Copyright (c) 2015 Marcin Ignac
+    // https://github.com/vorg/parse-hdr
+
+    const gammaPattern = 'GAMMA=\\s*([0-9]*[.][0-9]*)';
     const exposurePattern = 'EXPOSURE=\\s*([0-9]*[.][0-9]*)';
     const formatPattern = 'FORMAT=32-bit_rle_rgbe';
     const widthHeightPattern = '-Y ([0-9]+) \\\\+X ([0-9]+)';
@@ -42,35 +44,36 @@ const binpackLoader = binpackPath => `
         const buf = new Array(2);
         const bufferLength = buffer.length;
 
-        function readBuf(buf) {
+        function readBuffer(buf) {
             let bytesRead = 0;
             do {
-                buf[bytesRead++] = buffer[fileOffset];
-            } while(++fileOffset < bufferLength && bytesRead < buf.length);
+            buf[bytesRead++] = buffer[fileOffset];
+            } while (++fileOffset < bufferLength && bytesRead < buf.length);
             return bytesRead;
         }
 
-        function readBufOffset(buf, offset, length) {
+        function readBufferOffset(buf, offset, length) {
             let bytesRead = 0;
             do {
-                buf[offset + bytesRead++] = buffer[fileOffset];
-            } while(++fileOffset < bufferLength && bytesRead < length);
+            buf[offset + bytesRead++] = buffer[fileOffset];
+            } while (++fileOffset < bufferLength && bytesRead < length);
             return bytesRead;
         }
 
         function readPixelsRaw(buffer, data, offset, numpixels) {
             const numExpected = 4 * numpixels;
-            const numRead = readBufOffset(data, offset, numExpected);
+            const numRead = readBufferOffset(data, offset, numExpected);
         }
 
         while (numScanlines > 0) {
-            if ((rgbe[0] !== 2) || (rgbe[1] !== 2) || ((rgbe[2] & 0x80) !== 0)) {
-                    // this file is not run length encoded
+            if (rgbe[0] !== 2 || rgbe[1] !== 2 || (rgbe[2] & 0x80) !== 0) {
                 data[offset++] = rgbe[0];
                 data[offset++] = rgbe[1];
                 data[offset++] = rgbe[2];
                 data[offset++] = rgbe[3];
+
                 readPixelsRaw(buffer, data, offset, scanlineWidth * numScanlines - 1);
+                
                 return;
             }
 
@@ -79,55 +82,59 @@ const binpackLoader = binpackPath => `
             }
 
             ptr = 0;
-            /* read each of the four channels for the scanline into the buffer */
+
             for (let i = 0; i < 4; i++) {
                 ptrEnd = (i + 1) * scanlineWidth;
-                while(ptr < ptrEnd) {
-                    if (readBuf(buf) < buf.length) {
+
+                while (ptr < ptrEnd) {
+                    if (readBuffer(buf) < buf.length) {
                         throw new Error('Error reading 2-byte buffer');
                     }
-                    if ((buf[0] & 0xFF) > 128) {
-                        /* a run of the same value */
-                        count = (buf[0] & 0xFF) - 128;
-                        if ((count === 0) || (count > ptrEnd - ptr)) {
+
+                    if ((buf[0] & 0xff) > 128) {
+                        count = (buf[0] & 0xff) - 128;
+
+                        if (count === 0 || count > ptrEnd - ptr) {
                             throw new Error('Bad scanline data');
                         }
-                        while(count-- > 0) {
+
+                        while (count-- > 0) {
                             scanlineBuffer[ptr++] = buf[1];
                         }
                     } else {
-                        /* a non-run */
-                        count = buf[0] & 0xFF;
-                        if ((count === 0) || (count > ptrEnd - ptr)) {
+                        count = buf[0] & 0xff;
+
+                        if (count === 0 || count > ptrEnd - ptr) {
                             throw new Error('Bad scanline data');
                         }
+
                         scanlineBuffer[ptr++] = buf[1];
+
                         if (--count > 0) {
-                            if (readBufOffset(scanlineBuffer, ptr, count) < count) {
+                            if (readBufferOffset(scanlineBuffer, ptr, count) < count) {
                                 throw new Error('Error reading non-run data');
                             }
+
                             ptr += count;
                         }
                     }
                 }
             }
 
-            /* copy byte data to output */
-            for(let i = 0; i < scanlineWidth; i++) {
+            for (let i = 0; i < scanlineWidth; i++) {
                 data[offset + 0] = scanlineBuffer[i];
                 data[offset + 1] = scanlineBuffer[i + scanlineWidth];
                 data[offset + 2] = scanlineBuffer[i + 2 * scanlineWidth];
                 data[offset + 3] = scanlineBuffer[i + 3 * scanlineWidth];
+
                 offset += 4;
             }
 
             numScanlines--;
         }
-
     }
 
-    // Returns data as floats and flipped along Y by default
-    function parseHdr(buffer) {
+    function parseHDR(buffer) {
         if (buffer instanceof ArrayBuffer) {
             buffer = new Uint8Array(buffer);
         }
@@ -135,38 +142,40 @@ const binpackLoader = binpackPath => `
         let fileOffset = 0;
         const bufferLength = buffer.length;
 
-        const NEW_LINE = 10;
-
         function readLine() {
             let buf = '';
+
             do {
                 const b = buffer[fileOffset];
-                if (b === NEW_LINE) {
+
+                if (b === 10) {
                     ++fileOffset;
                     break;
                 }
+                
                 buf += String.fromCharCode(b);
-            } while(++fileOffset < bufferLength);
+            } while (++fileOffset < bufferLength);
+
             return buf;
         }
 
         let width = 0;
         let height = 0;
         let exposure = 1;
-        const gamma = 1;
+        let gamma = 1;
         let rle = false;
 
-        for(let i = 0; i < 20; i++) {
+        for (let i = 0; i < 20; i++) {
             const line = readLine();
             let match;
-            if (match = line.match(radiancePattern)) {
-            } else if (match = line.match(formatPattern)) {
+
+            if ((match = line.match(formatPattern))) {
                 rle = true;
-            } else if (match = line.match(exposurePattern)) {
+            } else if ((match = line.match(gammaPattern))) {
+                gamma = Number(match[1]);
+            } else if ((match = line.match(exposurePattern))) {
                 exposure = Number(match[1]);
-            } else if (match = line.match(commentPattern)) {
-            } else if (match = line.match(widthHeightPattern)) {
-                console.log(line);
+            } else if ((match = line.match(widthHeightPattern))) {
                 height = Number(match[1]);
                 width = Number(match[2]);
                 break;
@@ -184,7 +193,8 @@ const binpackLoader = binpackPath => `
         readPixelsRawRLE(buffer, data, 0, fileOffset, scanlineWidth, numScanlines);
 
         const floatData = new Float32Array(width * height * 4);
-        for(let offset = 0; offset < data.length; offset += 4) {
+
+        for (let offset = 0; offset < data.length; offset += 4) {
             let r = data[offset + 0] / 255;
             let g = data[offset + 1] / 255;
             let b = data[offset + 2] / 255;
@@ -204,12 +214,18 @@ const binpackLoader = binpackPath => `
         }
 
         return {
-            shape: [width, height],
-            exposure: exposure,
-            gamma: gamma,
-            data: floatData
+            width,
+            height,
+            exposure,
+            gamma,
+            data: floatData,
         };
     }
+`;
+
+// Webworker
+const binpackLoader = binpackPath => `
+    ${parseHDR}
 
     const BINPACKER_HEADER_MAGIC = 'BINP';
     const BINPACKER_HEADER_LENGTH = 12;
@@ -301,24 +317,17 @@ const binpackLoader = binpackPath => `
                 jsonChunk.map(
                     entry =>
                         new Promise((resolve, reject) => {
-                            const { name, mimeType } = entry;
+                            const { name } = entry;
                             const binary = binaryChunk.slice(entry.bufferStart, entry.bufferEnd);
                             
                             resolve({
                                 name,
-                                binary: parseHdr(new Uint8Array(binary)),
-                                mimeType,
+                                file: parseHDR(new Uint8Array(binary)),
                             })
                         }),
                 ),
             ).then((faces) => {
-                faces.map((face) => {
-                    console.log(face.binary)
-                });
-
-                self.postMessage({
-                    faces,
-                });
+                self.postMessage(faces);
 
                 self.close();
             });
