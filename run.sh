9eef529c00b4f11d1ae8549582c06bb124386c9a
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-09-17 09:20:37 +0200

    fix indentation

--- run.sh
+++ run.sh
@@ -5,101 +5,101 @@
 # Helper functions
 
 function log () {
-	echo -e "\033[36m"
-	echo "#########################################################"
-	echo "#### $1 "
-	echo "#########################################################"
-	echo -e "\033[m"
+  echo -e "\033[36m"
+  echo "#########################################################"
+  echo "#### $1 "
+  echo "#########################################################"
+  echo -e "\033[m"
 }
 
 # Tasks
 
 clean() {
-    PREFIX=$1
+  PREFIX=$1
 
-	rm -rf output/${PREFIX}_dfg.png
-	rm -rf output/${PREFIX}_sh_temp.txt
-	rm -rf output/${PREFIX}_sh.txt
-	rm -rf output/${PREFIX}_sh.js
-	rm -rf output/${PREFIX}_faces.binpack
-	rm -rf output/${PREFIX}
+  rm -rf output/${PREFIX}_dfg.png
+  rm -rf output/${PREFIX}_sh_temp.txt
+  rm -rf output/${PREFIX}_sh.txt
+  rm -rf output/${PREFIX}_sh.js
+  rm -rf output/${PREFIX}_faces.binpack
+  rm -rf output/${PREFIX}
 }
 
 convertSHTextToSHJavaScript() {
-    PREFIX=$1
+  PREFIX=$1
 
-	log "Converting spherical harmonics textfile to JavaScript version"
+  log "Converting spherical harmonics textfile to JavaScript version"
 
     # Modify a temporary file and keep the existing outputted text file
-	cp output/${PREFIX}_sh.txt output/${PREFIX}_sh_temp.txt
+  cp output/${PREFIX}_sh.txt output/${PREFIX}_sh_temp.txt
 
-	# Add two spaces in front of all lines, replace ( with [, ) with ] and remove trailing comma
-	sed -i '' -e 's/(/[/g' -e 's/);/],/g' -e 's/^/  /' output/${PREFIX}_sh_temp.txt
+  # Add two spaces in front of all lines, replace ( with [, ) with ] and remove trailing comma
+  sed -i '' -e 's/(/[/g' -e 's/);/],/g' -e 's/^/  /' output/${PREFIX}_sh_temp.txt
 
-	# Prepend array with export syntax to allow for easy importing in JavaScript
+  # Prepend array with export syntax to allow for easy importing in JavaScript
 
-	# module.exports = Float32Array.from([
-	#   [ 0.715154216008164,  0.750523596129484,  0.755632758209969],
-	#   [-0.483666219084928, -0.497031815163200, -0.632933219336340],
-	#   [-0.400305239028131, -0.380012952976089, -0.307701572177935],
-	#   [-0.257008604198698, -0.243140241833225, -0.203932262370727],
-	#   [ 0.278453300201378,  0.268009865292208,  0.235161043050550],
-	#   [ 0.454587099887602,  0.429839937567097,  0.368455742344463],
-	#   [ 0.154201891160878,  0.149757341174185,  0.109613647835982],
-	#   [ 0.302443299703856,  0.288082728879045,  0.241177035091168],
-	#   [-0.004275769928227, -0.000455848815188, -0.018626574489379],
-	# ]);
+  # module.exports = Float32Array.from([
+  #   [ 0.715154216008164,  0.750523596129484,  0.755632758209969],
+  #   [-0.483666219084928, -0.497031815163200, -0.632933219336340],
+  #   [-0.400305239028131, -0.380012952976089, -0.307701572177935],
+  #   [-0.257008604198698, -0.243140241833225, -0.203932262370727],
+  #   [ 0.278453300201378,  0.268009865292208,  0.235161043050550],
+  #   [ 0.454587099887602,  0.429839937567097,  0.368455742344463],
+  #   [ 0.154201891160878,  0.149757341174185,  0.109613647835982],
+  #   [ 0.302443299703856,  0.288082728879045,  0.241177035091168],
+  #   [-0.004275769928227, -0.000455848815188, -0.018626574489379],
+  # ]);
 
-	echo -e "module.exports = Float32Array.from([\n$(cat output/${PREFIX}_sh_temp.txt)" > output/${PREFIX}_sh.js
+  echo -e "module.exports = Float32Array.from([\n$(cat output/${PREFIX}_sh_temp.txt)" > output/${PREFIX}_sh.js
 
-	# Append a closing ]); after transformation
-	echo -e "]);" >> output/${PREFIX}_sh.js
+  # Append a closing ]); after transformation
+  echo -e "]);" >> output/${PREFIX}_sh.js
 
-	cat output/${PREFIX}_sh.js
+  cat output/${PREFIX}_sh.js
 
-    # Cleanup temp file
-    rm -rf output/${PREFIX}_sh_temp.txt
-    rm -rf output/${PREFIX}_sh.txt
+  # Cleanup temp file
+  rm -rf output/${PREFIX}_sh_temp.txt
+  rm -rf output/${PREFIX}_sh.txt
 }
 
 binpackFaces() {
-	PREFIX=$1
+  PREFIX=$1
 
-	log "Binpacking all outputted HDR faces"
+  log "Binpacking all outputted HDR faces"
 
-	npx binpacker -i output/${PREFIX} -o output/${PREFIX}_faces.binpack
+  npx binpacker -i output/${PREFIX} -o output/${PREFIX}_faces.binpack
 
-	rm -rf output/${PREFIX}
+  rm -rf output/${PREFIX}
 }
 
 run() {
-    PREFIX=$1
-    INPUT=$2
+  PREFIX=$1
+  INPUT=$2
 
-	log "Generating various files for image based lighting"
+  log "Generating various files for image based lighting"
 
-	# Clean up any existing old files under the same prefix
-	clean $PREFIX
+  # Clean up any existing old files under the same prefix
+  clean $PREFIX
 
-	# Output precalculated DFG LUT
-	# Output irradiance and pre-scaled base SH
-	# Output HDR image mips (m0 - m8), 256x256 to 1x1
+  # Output precalculated DFG LUT
+  # Output irradiance and pre-scaled base SH
+  # Output HDR image mips (m0 - m8), 256x256 to 1x1
     ./bin/cmgen \
-		--ibl-dfg=output/${PREFIX}_dfg.png \
-		--sh-irradiance \
-		--ibl-dfg-multiscatter \
-		--sh-shader \
-		--sh-output=output/${PREFIX}_sh.txt \
-		--ibl-ld=output \
-		--ibl-is-mipmap=output \
-		--format=hdr $INPUT
+    --ibl-dfg=output/${PREFIX}_dfg.png \
+    --sh-irradiance \
+    --ibl-dfg-multiscatter \
+    --sh-shader \
+    --sh-output=output/${PREFIX}_sh.txt \
+    --ibl-ld=output \
+    --ibl-is-mipmap=output \
+    --format=hdr $INPUT
 
-    # Convert outputted SH to a JavaScript file that can easily be interpreted
-	convertSHTextToSHJavaScript $PREFIX
+  # Convert outputted SH to a JavaScript file that can easily be interpreted
+  convertSHTextToSHJavaScript $PREFIX
 
-	# Pack all the individual faces into a single binpack binary
-	# https://www.npmjs.com/package/@timvanscherpenzeel/binpacker
-	binpackFaces $PREFIX
+  # Pack all the individual faces into a single binpack binary
+  # https://www.npmjs.com/package/@timvanscherpenzeel/binpacker
+  binpackFaces $PREFIX
 }
 
 # Main script
--- setup_macos.sh
+++ setup_macos.sh
@@ -5,82 +5,82 @@
 # Helper functions
 
 function log () {
-	echo -e "\033[36m"
-	echo "#########################################################"
-	echo "#### $1 "
-	echo "#########################################################"
-	echo -e "\033[m"
+  echo -e "\033[36m"
+  echo "#########################################################"
+  echo "#### $1 "
+  echo "#########################################################"
+  echo -e "\033[m"
 }
 
 setup() {
-    UNAME=$(uname)
+  UNAME=$(uname)
 
-    if [ "$UNAME" != "Darwin" ]; then
-        echo "Currently only MacOS is supported by this automatic setup script"
-        exit 1
-    fi
+  if [ "$UNAME" != "Darwin" ]; then
+      echo "Currently only MacOS is supported by this automatic setup script"
+      exit 1
+  fi
 
-    # Node.js is required for the @timvanscherpenzeel/binpacker package
-    node --version | grep "v" &> /dev/null
+  # Node.js is required for the @timvanscherpenzeel/binpacker package
+  node --version | grep "v" &> /dev/null
 
-    if [ $? != 0 ]; then
-        echo "Please make sure to install Node.js <https://nodejs.org/en/download/>"
-        exit 1
-    fi
+  if [ $? != 0 ]; then
+      echo "Please make sure to install Node.js <https://nodejs.org/en/download/>"
+      exit 1
+  fi
 
-    # Install `npm` dependencies, prefer using Yarn
-    yarn --version &> /dev/null
+  # Install `npm` dependencies, prefer using Yarn
+  yarn --version &> /dev/null
 
-    if [ $? == 0 ]; then
-        echo "Installing NPM packages using Yarn"
-        yarn
-    else
-        echo "Installing NPM packages using NPM"
-        npm install --no-package-lock
-    fi
+  if [ $? == 0 ]; then
+      echo "Installing NPM packages using Yarn"
+      yarn
+  else
+      echo "Installing NPM packages using NPM"
+      npm install --no-package-lock
+  fi
 
-    # Clean up any old 'cmgen' binaries
-    rm -rf ./bin/cmgen
+  # Clean up any old 'cmgen' binaries
+  rm -rf ./bin/cmgen
 
-    # Create input, output and bin directories
-    log "Creating 'input', 'output' and 'bin' directories"
+  # Create input, output and bin directories
+  log "Creating 'input', 'output' and 'bin' directories"
 
-    mkdir -p input
-    mkdir -p output
-    mkdir -p bin
+  mkdir -p input
+  mkdir -p output
+  mkdir -p bin
 
-    echo "Succesfully created 'input', 'output' and 'bin' directories"
+  echo "Succesfully created 'input', 'output' and 'bin' directories"
 
-	# Grab the latest Filament MacOS stable release version (Darwin)
-    log "Downloading the latest Filament release"
+  # Grab the latest Filament MacOS stable release version (Darwin)
+  log "Downloading the latest Filament release"
 
-    # Latest stable release
-    # FILAMENT_URL=$(curl -s https://api.github.com/repos/google/filament/releases/latest | jq -r ".assets[] | select(.name | test(\"darwin\")) | .browser_download_url")
+  # Latest stable release
+  # FILAMENT_URL=$(curl -s https://api.github.com/repos/google/filament/releases/latest | jq -r ".assets[] | select(.name | test(\"darwin\")) | .browser_download_url")
 
-    # Latest nightly release
-    FILAMENT_URL=$(curl https://filament-build.storage.googleapis.com/badges/build_link_mac.html | grep -ioE "https://.*.tgz")
+  # Latest nightly release
+  FILAMENT_URL=$(curl https://filament-build.storage.googleapis.com/badges/build_link_mac.html | grep -ioE "https://.*.tgz")
 
-    # Fetch the latest Filament MacOS stable release version (Darwin)
-    wget "$FILAMENT_URL" -P bin -O ./bin/filament.tgz
+  # Fetch the latest Filament MacOS stable release version (Darwin)
+  wget "$FILAMENT_URL" -P bin -O ./bin/filament.tgz
 
-    log "Validating the downloaded compressed Filament archive"
+  log "Validating the downloaded compressed Filament archive"
 
-    # Validate the retrieved tgz archive (does not check the integrity of the file)
-    gzip -t ./bin/filament.tgz && echo "Filament was retrieved correctly and the archive is valid" || $(echo "Failed to retrieve Filament correctly and the archive is corrupted"; exit 1)
+  # Validate the retrieved tgz archive (does not check the integrity of the file)
+  gzip -t ./bin/filament.tgz && echo "Filament was retrieved correctly and the archive is valid" || $(echo "Failed to retrieve Filament correctly and the archive is corrupted"; exit 1)
 
-    log "Extracing 'cmgen' from latest Filament release"
+  log "Extracing 'cmgen' from latest Filament release"
 
-    # Extract the compressed archive
-    tar -xvzf ./bin/filament.tgz -C bin
+  # Extract the compressed archive
+  tar -xvzf ./bin/filament.tgz -C bin
 
-    # Move the `cmgen` binary into in main `bin` directory
-    mv ./bin/filament/bin/cmgen bin
+  # Move the `cmgen` binary into in main `bin` directory
+  mv ./bin/filament/bin/cmgen bin
 
-    echo -e "\nSuccesfully extracted 'cmgen' into the 'bin' directory"
+  echo -e "\nSuccesfully extracted 'cmgen' into the 'bin' directory"
 
-    # Clean Filament directory and compressed directory
-    rm -rf ./bin/filament
-    rm -rf ./bin/filament.tgz
+  # Clean Filament directory and compressed directory
+  rm -rf ./bin/filament
+  rm -rf ./bin/filament.tgz
 }
 
 # Main script
